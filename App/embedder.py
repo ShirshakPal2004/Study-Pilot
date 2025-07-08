f805e898ed1dc2f767959bd9a4b267f61565117c
ShirshakPal2004 <145149840+ShirshakPal2004@users.noreply.github.com>
2025-07-08 13:15:37 +0530

    Update embedder.py

--- App/embedder.py
+++ App/embedder.py
@@ -6,3 +6,6 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def embed_text(chunks: List[str]) -> np.ndarray:
     return model.encode(chunks)
+    
+def embed_question(question: str) -> np.ndarray:
+    return model.encode([question])
